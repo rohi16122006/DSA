1a6d3eb3a59e6b0ee62fe7753740b9d2cf734ada
rohi16122006 <rohithyadavkb@gmail.com>
2026-09-12 14:39:37 +0530

    Time: 4 ms (96.29%) | Memory: 49.6 MB (42.16%) - LeetSync

---
+++ 816-design-hashset/design-hashset.cpp
@@ -0,0 +1,16 @@
+class MyHashSet {
+public:
+    vector<bool> present;
+    MyHashSet() {
+        present.resize(1000001, false);
+    }
+    void add(int key) {
+        present[key] = true;
+    }
+    void remove(int key) {
+        present[key] = false;
+    }
+    bool contains(int key) {
+        return present[key];
+    }
+};
