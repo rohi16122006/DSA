01a40b8882129ed881c6396d7d44bd23398639cd
rohi16122006 <rohithyadavkb@gmail.com>
2026-09-13 22:45:57 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (74.02%) - LeetSync

---
+++ 228-summary-ranges/summary-ranges.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<string> summaryRanges(vector<int>& nums) {
+        vector<string> ans;
+        int i = 0;
+        while (i < nums.size()) {
+            int start = nums[i];
+            while (i + 1 < nums.size() && nums[i + 1] == nums[i] + 1) {
+                i++;
+            }
+            if (start == nums[i]) {
+                ans.push_back(to_string(start));
+            }
+            else {
+                ans.push_back(to_string(start) + "->" + to_string(nums[i]));
+            }
+            i++;
+        }
+        return ans;
+    }
+};
